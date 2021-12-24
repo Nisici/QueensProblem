2a96fa4ecb845dc8e0812e96f2c67e9ad78bdbbe
Nisici <56770874+Nisici@users.noreply.github.com>
2021-12-24 13:05:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import QueensProblems as qs
+import MinConflicts as mc
+
 # test solving n queens problem using min conflicts algorithm
 # steps: maximum number of steps before exiting
 # it can print the initial and final grid, useful only if n is small.
 if __name__ == '__main__':
-    steps = 100000
-    Q = qs.csp_queens(500)
+    steps = 1000
+    Q = qs.csp_queens(8)
     print("INITIAL STATE : ")
     Q.printGrid(Q.initialValues)
-    ris_state = min_conflicts(Q,steps)
+    ris_state = mc.min_conflicts(Q,steps)
     print("FINAL STATE : ")
     if ris_state:
         Q.printGrid(ris_state)
